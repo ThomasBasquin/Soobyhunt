36f8fbbeab5777503e86ea964e9e1a39103d3559
Thomas <thomas.basquin2@gmail.com>
2023-02-03 15:51:18 +0100

    react-native-maps

--- scoobyflag_mobile/src/Pages/Home/Home.jsx
+++ scoobyflag_mobile/src/Pages/Home/Home.jsx
@@ -1,13 +1,26 @@
-import { View, Text, Button } from "react-native";
+import {View, Text, Button} from 'react-native';
+import {UrlTile} from 'react-native-maps';
+import MapView from 'react-native-maps';
 
+export default function Home({navigation}) {
+  return (
+    <View>
+      <MapView
+        style={{height: '100%', width: '100%'}}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+        <UrlTile
+          //   urlTemplate="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          maximumZ={19}
+        />
+      </MapView>
 
-export default function Home({navigation}){
-
-    return (
-        <View>
-            <Text>Envoie fesse</Text>
-            <Button title="Signin" onPress={()=> navigation.navigate('Signin')}  />
-            <Button title="Signup" onPress={()=> navigation.navigate('Signup')}  />
-        </View>
-    )
+      <Button title="Signin" onPress={() => navigation.navigate('Signin')} />
+      <Button title="Signup" onPress={() => navigation.navigate('Signup')} />
+    </View>
+  );
 }
