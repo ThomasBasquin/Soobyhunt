fab9cafa50e0575014bfafeacdc8be1562e9ff79
rprobst <romeo.probst@etu.unistra.fr>
2023-02-09 16:29:40 +0100

    Page config

--- scoobyflag_web/src/route/Config.jsx
+++ scoobyflag_web/src/route/Config.jsx
@@ -1,8 +1,34 @@
 import { Link } from "react-router-dom";
-import "../css/config.css"
+import "../css/config.css";
 
 export default function Config() {
-    return (<div className="fondViolet">
-        <h1>Configuration d'une partie</h1>
-    </div>)
+  return (
+    <div className="fondViolet">
+      <h1 className="titreConfig">Configuration d'une partie</h1>
+      <div className="formConfig">
+        <ModeDeJeu />
+      </div>
+    </div>
+  );
+}
+
+const ModeDeJeu = () => {
+  return (
+    <div className="modeDeJeu">
+      <p className="choixMDJ">Choix du mode de jeu :</p>
+      <div className="listMDJ">
+        <ItemMDJ />
+        <ItemMDJ />
+        <ItemMDJ />
+      </div>
+    </div>
+  );
+};
+
+const ItemMDJ = () => {
+    return (
+        <div className="">
+            cc
+        </div>
+    )
 }
