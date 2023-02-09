c340749bf235e45821091e97a68c1e6f7f5e42fa
rprobst <romeo.probst@etu.unistra.fr>
2023-02-09 15:48:43 +0100

    Rajout link header

--- scoobyflag_web/src/components/Header.jsx
+++ scoobyflag_web/src/components/Header.jsx
@@ -3,6 +3,6 @@ import "../css/header.css";
 
 export default function Header() {
     return <header>
-        <Link to="" className='active' id='home'>Accueil</Link> <Link to="/dashboard/map" id='map' className='link'>Map</Link> {/* <Link to="messages" id='messages' className='link'>Messages</Link> <Link to="user" id='user' className='link'>Utilisateur</Link>*/}
+        <Link to="" className='active' id='home'>Accueil</Link> <Link to="/dashboard/map" id='map' className='link'>Map</Link> <Link to="/dashboard/config" id='config' className='link'>Config</Link> {/* <Link to="messages" id='messages' className='link'>Messages</Link> <Link to="user" id='user' className='link'>Utilisateur</Link>*/}
     </header>
 }
