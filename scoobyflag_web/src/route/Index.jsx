1645f20f0f0392f642fee1d5d8b1eb73d64aced4
rprobst <romeo.probst@etu.unistra.fr>
2023-02-09 12:01:50 +0100

    Design page Accueil

--- scoobyflag_web/src/route/Index.jsx
+++ scoobyflag_web/src/route/Index.jsx
@@ -3,8 +3,10 @@ import "../css/main.css";
 
 export default function Index() {
     return (<div className="fondSamy">
+        <div className="infoAccueil">
         <div className="titre">CTF Grandeur nature !</div>
         <div className="presentation">Venez joueur sur le meilleur jeu de capture the Flag grandeur nature sur le thème Scooby-Doo</div>
         <Link className="createGame" to="/dashboard">Créer une partie</Link>
+        </div>
     </div>)
 }
