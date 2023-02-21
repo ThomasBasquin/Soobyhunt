d998bf410fa58fd442487877e461ba06138f54bd
PROBST Roméo <348717@cus.fr>
2023-02-21 13:24:13 +0100

    Ajout du composant Add Equipe

--- scoobyflag_web/src/route/Config.jsx
+++ scoobyflag_web/src/route/Config.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import "../css/config.css";
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 export default function Config() {
   return (
@@ -16,101 +16,171 @@ export default function Config() {
 }
 
 const Contenu = () => {
-   const [toggleState, setToggleState] = useState(1);
-   const [choose, setChoose] = useState("");
+  const [toggleState, setToggleState] = useState(1);
+  const [choose, setChoose] = useState("");
   const toggleTab = (index) => {
     setToggleState(index);
   };
 
   const chooseItem = (type) => {
-    setChoose(type)
-  }
+    setChoose(type);
+  };
   return (
     <div className="">
       <div className="listEcran">
-        <div className={toggleState === 1 ? "choixMDJ active-tab" : "choixMDJ"} onClick={() => toggleTab(1)}>Mode de jeu</div>
-        <div className={toggleState === 2 ? "choixMDJ active-tab" : "choixMDJ"} onClick={() => toggleTab(2)}>Équipe</div>
+        <div
+          className={toggleState === 1 ? "choixMDJ active-tab" : "choixMDJ"}
+          onClick={() => toggleTab(1)}
+        >
+          Mode de jeu
         </div>
-        <div className="listEcran">
-        <div className={toggleState === 3 ? "choixMDJ active-tab" : "choixMDJ"} onClick={() => toggleTab(3)}>Méchants</div>
-        <div className={toggleState === 4 ? "choixMDJ active-tab" : "choixMDJ"} onClick={() => toggleTab(4)}>Items</div>
+        <div
+          className={toggleState === 2 ? "choixMDJ active-tab" : "choixMDJ"}
+          onClick={() => toggleTab(2)}
+        >
+          Équipes
+        </div>
+      </div>
+      <div className="listEcran">
+        <div
+          className={toggleState === 3 ? "choixMDJ active-tab" : "choixMDJ"}
+          onClick={() => toggleTab(3)}
+        >
+          Méchants
+        </div>
+        <div
+          className={toggleState === 4 ? "choixMDJ active-tab" : "choixMDJ"}
+          onClick={() => toggleTab(4)}
+        >
+          Items
+        </div>
       </div>
 
-      <ModeDeJeu choose={choose} chooseItem={chooseItem} toggleState={toggleState} />
-      <div className={toggleState === 2? "listMDJ content active-content" : "listMDJ content"}>
-        <Equipe />
+      <ModeDeJeu
+        choose={choose}
+        chooseItem={chooseItem}
+        toggleState={toggleState}
+      />
+      <div
+        className={
+          toggleState === 2
+            ? "listMDJ content active-content"
+            : "listMDJ content"
+        }
+      >
+        <Equipes />
       </div>
-      <div className={toggleState === 3? "listMDJ content active-content" : "listMDJ content"}><Mechants /></div>
-      <div className={toggleState === 4? "listMDJ content active-content" : "listMDJ content"}><Items /></div>
+      <div
+        className={
+          toggleState === 3
+            ? "listMDJ content active-content"
+            : "listMDJ content"
+        }
+      >
+        <Mechants />
+      </div>
+      <div
+        className={
+          toggleState === 4
+            ? "listMDJ content active-content"
+            : "listMDJ content"
+        }
+      >
+        <Items />
+      </div>
     </div>
   );
 };
-const ModeDeJeu = ({choose, chooseItem, toggleState}) => {
+const ModeDeJeu = ({ choose, chooseItem, toggleState }) => {
   return (
-<div className={toggleState === 1 ? "listMDJ content active-content" : "listMDJ content"}>
-      <div onClick={() => chooseItem("time")} className={choose === "time"? "itemMDJ selected-item" : "itemMDJ"}>
-          <img
-            src="chrono.jpeg"
-            style={{ width: 140, height: 140, borderRadius: 30 }}
-          />
-          <div className="" style={{ marginLeft: 10, marginRight: 10 }}>
-            <h2 style={{ margin: 0, marginTop: 10 }}>TIME</h2>
-            <p>
-              L’équipe gagnante est celle qui a le temps cumulé de possession
-              des drapeaux le plus important à la fin de la partie
-            </p>
-          </div>
+    <div
+      className={
+        toggleState === 1 ? "listMDJ content active-content" : "listMDJ content"
+      }
+    >
+      <div
+        onClick={() => chooseItem("time")}
+        className={choose === "time" ? "itemMDJ selected-item" : "itemMDJ"}
+      >
+        <img
+          src="chrono.jpeg"
+          style={{ width: 140, height: 140, borderRadius: 30 }}
+        />
+        <div className="" style={{ marginLeft: 10, marginRight: 10 }}>
+          <h2 style={{ margin: 0, marginTop: 10 }}>TIME</h2>
+          <p>
+            L’équipe gagnante est celle qui a le temps cumulé de possession des
+            drapeaux le plus important à la fin de la partie
+          </p>
         </div>
-        <div onClick={() => chooseItem("flag")} className={choose === "flag"? "itemMDJ selected-item" : "itemMDJ"}>
-          <img
-            src="flag.jpeg"
-            style={{ width: 140, height: 140, borderRadius: 30 }}
-          />
-          <div className="" style={{ marginLeft: 10, marginRight: 10 }}>
-            <h2>FLAG</h2>
-            <p>
-              L’équipe gagnante est celle qui possède le plus de drapeaux à la
-              fin de la partie
-            </p>
-          </div>
-        </div>
-        <div onClick={() => chooseItem("supremacy")} className={choose === "supremacy"? "itemMDJ selected-item" : "itemMDJ"}>
-          <img
-            src="supremacy.jpeg"
-            style={{ width: 140, height: 140, borderRadius: 30 }}
-          />
-          <div className="" style={{ marginLeft: 10, marginRight: 10 }}>
-            <h2>SUPREMACY</h2>
-            <p>
-              {" "}
-              L’équipe gagnante est la première à posséder la majorité des
-              drapeaux
-            </p>
-          </div>
-        </div>
-        
       </div>
-  )
-}
-const Equipe = () => {
-  return (
-    <div>
-      cc
+      <div
+        onClick={() => chooseItem("flag")}
+        className={choose === "flag" ? "itemMDJ selected-item" : "itemMDJ"}
+      >
+        <img
+          src="flag.jpeg"
+          style={{ width: 140, height: 140, borderRadius: 30 }}
+        />
+        <div className="" style={{ marginLeft: 10, marginRight: 10 }}>
+          <h2>FLAG</h2>
+          <p>
+            L’équipe gagnante est celle qui possède le plus de drapeaux à la fin
+            de la partie
+          </p>
+        </div>
+      </div>
+      <div
+        onClick={() => chooseItem("supremacy")}
+        className={choose === "supremacy" ? "itemMDJ selected-item" : "itemMDJ"}
+      >
+        <img
+          src="supremacy.jpeg"
+          style={{ width: 140, height: 140, borderRadius: 30 }}
+        />
+        <div className="" style={{ marginLeft: 10, marginRight: 10 }}>
+          <h2>SUPREMACY</h2>
+          <p>
+            {" "}
+            L’équipe gagnante est la première à posséder la majorité des
+            drapeaux
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
-const Mechants = () => {
+  );
+};
+const Equipes = () => {
+  return <div>
+    <p style={{backgroundColor : "var(--violet)", width: 120, color :"var(--gris)", padding : 10, borderRadius :15}}>Nouvelle équipe</p>
+    <AddEquipe />
+    
+  </div>;
+};
+
+const AddEquipe = () => {
   return (
-    <div>
-      cc
-    </div>
-  )
-}
-const Items = () => {
-  return (
-    <div>
-      cc
+    <div className="addEquipe">
+<div>
+      <input type="text" name='nomEquipe' className="nomEquipe" placeholder="Nom de l'équipe" />
+      <input type="number" name="nbJoueur" className="nbJoueur" min="0"/>Joueurs
+      </div>
+      <p style={{backgroundColor : "var(--violet)", width: 50, color :"var(--gris)", padding : 10, borderRadius :15}}>Ajouter</p>
     </div>
   )
 }
 
+
+
+
+
+
+
+
+
+const Mechants = () => {
+  return <div>cc</div>;
+};
+const Items = () => {
+  return <div>cc</div>;
+};
