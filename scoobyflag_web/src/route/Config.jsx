94e2c9916732047166b4899ccb207bed81745f17
rprobst <romeo.probst@etu.unistra.fr>
2023-02-24 15:38:32 +0100

    partie mechant config finie

--- scoobyflag_web/src/route/Config.jsx
+++ scoobyflag_web/src/route/Config.jsx
@@ -34,6 +34,9 @@ async function createGame() {
 const Contenu = () => {
   const [toggleState, setToggleState] = useState(1);
   const [choose, setChoose] = useState("");
+
+  const [nbGrandMechant, setnbGrandMechant] = useState(1);
+  const [nbPetitMechant, setnbPetitMechant] = useState(1);
   const toggleTab = (index) => {
     setToggleState(index);
   };
@@ -93,7 +96,12 @@ const Contenu = () => {
             : "listMDJ content"
         }
       >
-        <Mechants />
+        <Mechants
+          nbGrandMechant={nbGrandMechant}
+          nbPetitMechant={nbPetitMechant}
+          setnbGrandMechant={setnbGrandMechant}
+          setnbPetitMechant={setnbPetitMechant}
+        />
       </div>
       <div
         className={
@@ -167,70 +175,191 @@ const ModeDeJeu = ({ choose, chooseItem, toggleState }) => {
   );
 };
 const Equipes = () => {
-
-  const [listeEquipe, setListeEquipe] = useState([{
-    id :1,
-    nom: "equipe1",
-    nbJoueur: 4,
-
-  },
-  {
-    id :2,
-    nom: "equipe2",
-    nbJoueur: 8,
-  }
-]);
-  return <div>
-    
-    <AjoutEquipe listeEquipe={listeEquipe}/>
-    <ListeEquipe listeEquipe={listeEquipe} setListeEquipe={setListeEquipe}/>
-    
-  </div>;
+  const [listeEquipe, setListeEquipe] = useState([
+    {
+      id: 1,
+      nom: "equipe1",
+      nbJoueur: 4,
+    },
+    {
+      id: 2,
+      nom: "equipe2",
+      nbJoueur: 8,
+    },
+  ]);
+  return (
+    <div>
+      <AjoutEquipe listeEquipe={listeEquipe} />
+      <ListeEquipe listeEquipe={listeEquipe} setListeEquipe={setListeEquipe} />
+    </div>
+  );
 };
 
-const AjoutEquipe = ({listeEquipe}) => {
-  const [nomEquipe, setNomEquipe]= useState("");
-  const [nbJoueur, setNbJoueur]= useState(1);
-  const [id, setId]= useState(3);
+const AjoutEquipe = ({ listeEquipe }) => {
+  const [nomEquipe, setNomEquipe] = useState("");
+  const [nbJoueur, setNbJoueur] = useState(1);
+  const [id, setId] = useState(3);
 
   const ajout = () => {
-    setId(id+1);
-    listeEquipe.push({id: '1', nom : 'cc', nbJoueur : '7'});
-  }
+    setId(id + 1);
+    listeEquipe.push({ id: "1", nom: "cc", nbJoueur: "7" });
+  };
   return (
     <div className="addEquipe">
-<div>
-      <input type="text" name='nomEquipe' className="nomEquipe" placeholder="Nom de l'équipe" value={nomEquipe} onChange={e => setNomEquipe(e.target.value)}/>
-      <input type="number" name="nbJoueur" className="nbJoueur" min="1" value={nbJoueur} onChange={e => setNbJoueur(e.target.value)}/>Joueurs
+      <div>
+        <input
+          type="text"
+          name="nomEquipe"
+          className="nomEquipe"
+          placeholder="Nom de l'équipe"
+          value={nomEquipe}
+          onChange={(e) => setNomEquipe(e.target.value)}
+        />
+        <input
+          type="number"
+          name="nbJoueur"
+          className="nbJoueur"
+          min="1"
+          value={nbJoueur}
+          onChange={(e) => setNbJoueur(e.target.value)}
+        />
+        Joueurs
       </div>
-      <p onClick = {()=> ajout()} style={{backgroundColor : "var(--violet)", width: 50, color :"var(--gris)", padding : 10, borderRadius :15, cursor : "pointer"}}>Ajouter</p>
+      <p
+        onClick={() => ajout()}
+        style={{
+          backgroundColor: "var(--violet)",
+          width: 50,
+          color: "var(--gris)",
+          padding: 10,
+          borderRadius: 15,
+          cursor: "pointer",
+        }}
+      >
+        Ajouter
+      </p>
     </div>
-  )
-}
+  );
+};
 
-
-
-const ListeEquipe = ({listeEquipe, setListeEquipe}) => {
+const ListeEquipe = ({ listeEquipe, setListeEquipe }) => {
   return (
     <div className="listeEquipe">
-      {listeEquipe==0 ? <p style={{fontSize : 30}}>Aucune équipe pour le moment</p> : listeEquipe.map((e)=> {
-        return (
-          <div key={e.id} className="equipe">
-            <p>{e.nom}</p>
-            <p>{e.nbJoueur}</p>
-          </div>
-        )
-      })}
+      {listeEquipe == 0 ? (
+        <p style={{ fontSize: 30 }}>Aucune équipe pour le moment</p>
+      ) : (
+        listeEquipe.map((e) => {
+          return (
+            <div key={e.id} className="equipe">
+              <p>{e.nom}</p>
+              <p>{e.nbJoueur}</p>
+            </div>
+          );
+        })
+      )}
     </div>
-  )
-}
+  );
+};
 
-
-
-
-
-const Mechants = () => {
-  return <div>cc</div>;
+const Mechants = ({
+  nbGrandMechant,
+  nbPetitMechant,
+  setnbGrandMechant,
+  setnbPetitMechant,
+}) => {
+  console.log(nbGrandMechant);
+  return (
+    <div className="listeMechant">
+      <div
+        className="itemMDJ"
+        style={{ margin: 20, padding: 10, marginLeft: 0, cursor: "auto" }}
+      >
+        <img
+          src="mechant2.png"
+          style={{ width: 140, height: 140, borderRadius: 30 }}
+        />
+        <div className="" style={{ marginLeft: 10, marginRight: 10 }}>
+          <h2>Grand Méchant</h2>
+          <p>
+            Difficulté : <span style={{ color: "orange" }}>Moyenne</span>
+          </p>
+          <p style={{ display: "flex", justifyContent: "space-around" }}>
+            <span
+              style={{
+                backgroundColor: "var(--violet)",
+                color: "var(--gris)",
+                padding: 5,
+                fontSize: 18,
+                borderRadius: 100,
+                cursor: "pointer",
+              }}
+              onClick={() => setnbGrandMechant(nbGrandMechant - 1)}
+            >
+              -
+            </span>{" "}
+            <span style={{ padding: 10 }}>{nbGrandMechant} </span>
+            <span
+              style={{
+                backgroundColor: "var(--violet)",
+                color: "var(--gris)",
+                padding: 5,
+                fontSize: 18,
+                borderRadius: 100,
+                cursor: "pointer",
+              }}
+              onClick={() => setnbGrandMechant(nbGrandMechant + 1)}
+            >
+              +
+            </span>
+          </p>
+        </div>
+      </div>
+      <div
+        className="itemMDJ"
+        style={{ margin: 20, padding: 10, marginLeft: 0, cursor: "auto" }}
+      >
+        <img
+          src="mechant1.png"
+          style={{ width: 140, height: 140, borderRadius: 30 }}
+        />
+        <div className="" style={{ marginLeft: 10, marginRight: 10 }}>
+          <h2>Petit Méchant</h2>
+          <p>
+            Difficulté : <span style={{ color: "red" }}>Difficile</span>
+          </p>
+          <p style={{ display: "flex", justifyContent: "space-around" }}>
+            <span
+              style={{
+                backgroundColor: "var(--violet)",
+                color: "var(--gris)",
+                padding: 5,
+                fontSize: 18,
+                borderRadius: 100,
+                cursor: "pointer",
+              }}
+              onClick={() => setnbPetitMechant(nbPetitMechant - 1)}
+            >
+              -
+            </span>{" "}
+            <span style={{ padding: 10 }}>{nbPetitMechant} </span>
+            <span
+              style={{
+                backgroundColor: "var(--violet)",
+                color: "var(--gris)",
+                padding: 5,
+                fontSize: 18,
+                borderRadius: 100,
+                cursor: "pointer",
+              }}
+              onClick={() => setnbPetitMechant(nbPetitMechant + 1)}
+            >
+              +
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 const Items = () => {
   return <div>cc</div>;
