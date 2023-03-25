24b466c9111a2b69a0d609cfddb1786380bcb336
hugos <schellinger@c-koya.tech>
2023-03-25 15:34:54 +0100

    abonnement mercure fonctionne

--- scoobyflag_mobile/src/App.tsx
+++ scoobyflag_mobile/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Provider} from 'react-redux';
 import RouteProvider from './Routes/RouteProvider';
 import {PersistGate} from 'redux-persist/integration/react';
@@ -6,17 +6,20 @@ import {persistor, store} from './Services/store';
 import RNEventSource from 'react-native-event-source';
 
 function App() {
-  const uri = encodeURIComponent('https://example.com/users/dunglas');
-  try {
+
+  useEffect(() => {
+    const uri = encodeURIComponent('toto');
     const eventSource = new RNEventSource(
       'http://82.165.109.36/.well-known/mercure?topic=' + uri,
     );
+    console.log("test");
+    
     eventSource.addEventListener('message', (event: any) => {
-      console.log(event.type);
-      console.log(event.data);
+      console.log(event);
     });
-  } catch (e) {
-  }
+
+    return () => eventSource.close();
+  }, []);
 
   return (
     <Provider store={store}>
