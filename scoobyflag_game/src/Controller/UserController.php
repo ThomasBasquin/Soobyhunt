6357adff0caa25efb33c7ed74f6adacc3216d332
gaetanfallot67 <fallot@c-koya.tehc>
2023-04-13 09:06:28 +0200

    [gameServer] Mise en place doc

--- scoobyflag_game/src/Controller/UserController.php
+++ scoobyflag_game/src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Team;
 use App\Entity\User;
 use App\Service\UserService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,6 +14,7 @@ use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
 use OpenApi\Attributes as OA;
+// #[OA\Tag(name: 'userController')]
 #[Route('/user', name: 'user')]
 class UserController extends AbstractController
 {
@@ -29,21 +31,7 @@ class UserController extends AbstractController
     }
 
     #[Route('/{user}/position', name: 'update_position', methods: ['GET'])]
-    #[OA\Response(
-        response: 200,
-        description: 'Retourne la position des users et ce que l\'utilisateur voit',
-        content: new OA\JsonContent(
-            type: 'array',
-            items: new OA\Items(ref: new Model( groups: ["User:read", "Objective:read", "Item:read"]))
-        )
-    )]
-    #[OA\Parameter(
-        name: 'user_update_position',
-        in: 'header',
-        description: 'Sauvegarde la position et revoie dans mercure ce qu\il voit',
-        schema: new OA\Schema(type: 'string')
-    )]
-    #[OA\Tag(name: 'user_update_position')]
+    
     public function updatePosition(HubInterface $hub, User $user/*, Request $request*/): Response
     {
         // $data = $request->toArray();
@@ -64,21 +52,7 @@ class UserController extends AbstractController
     }
 
     #[Route('/join', name: 'join', methods: ['POST'])]
-    #[OA\Response(
-        response: 200,
-        description: 'Créer un user en fonction d\'un user central',
-        content: new OA\JsonContent(
-            type: 'array',
-            items: new OA\Items(ref: new Model(type:User::class, groups: ["User:read"]))
-        )
-    )]
-    #[OA\Parameter(
-        name: 'user_join',
-        in: 'header',
-        description: 'Créer un user en fonction de ce qu\'on renvoie',
-        schema: new OA\Schema(type: 'string')
-    )]
-    #[OA\Tag(name: 'user_join')]
+   
     public function join(HubInterface $hub, Request $request): Response
     {
         $data = $request->toArray();
@@ -106,6 +80,24 @@ class UserController extends AbstractController
         $data = $request->toArray();
 
         $user->setIsReady(!$user->isIsReady());
+        $this->em->persist($user);
+        $this->em->flush();
+
+        // $update = new Update(
+        //     'https://example.com/users/dunglas',
+        //     json_encode(['status' => 'OutOfStock'])
+        // );
+
+        // $hub->publish($update);
+
+        return $this->json([$user], 200, [], ['groups' => ["User:read"]]);
+    }
+
+    #[Route('/{user}/team/{team}', name: 'is_ready', methods: ['PUT'])]
+    public function changeTeam(HubInterface $hub, User $user, Team $team): Response
+    {
+
+        $user->setTeam($team);
         $this->em->persist($user);
         $this->em->flush();
 
