83bd98d9c666ba5300ed576d95496b6c415dc7aa
Thomas <thomas.basquin2@gmail.com>
2023-05-04 12:45:41 +0200

    Conteneur mercure fonctionnel

--- scoobyflag_game/launch-instance.sh
+++ scoobyflag_game/launch-instance.sh
@@ -6,6 +6,20 @@ DB_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 DB_PORT=$(shuf -i 3307-3399 -n 1)
 HOST_PORT=$(shuf -i 8000-9000 -n 1)
 
+MERCURE_PUBLISHER_JWT_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+MERCURE_SUBSCRIBER_JWT_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+APP_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+MERCURE_JWT_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+
+generate_mercure_port() {
+  MERCURE_PORT=$(shuf -i 9001-9999 -n 1)
+  while docker ps --format '{{.Ports}}' | grep -q "${MERCURE_PORT}->80/tcp"; do
+    MERCURE_PORT=$(shuf -i 9001-9999 -n 1)
+  done
+}
+generate_mercure_port
+
 # Write values to .env file
 echo "DB_NAME=${DB_NAME}" > .env
 echo "DB_USER=${DB_USER}" >> .env
@@ -13,10 +27,15 @@ echo "DB_PASSWORD=${DB_PASSWORD}" >> .env
 echo "DB_PORT=${DB_PORT}" >> .env
 echo "HOST_PORT=${HOST_PORT}" >> .env
 echo "DATABASE_URL=mysql://${DB_USER}:${DB_PASSWORD}@database:${DB_PORT}/${DB_NAME}" >> .env
-echo "MERCURE_URL=http://mercure:16640/." >> .env
-echo "MERCURE_PUBLIC_URL=http://hugoslr.fr:16640/.well-known/mercure" >> .env
-echo "MERCURE_JWT_SECRET='!ChangeThisMercureHubJWTSecretKey!'" >> .env
-
+# echo "MERCURE_PUBLIC_URL=http://localhost:${MERCURE_PORT}/.well-known/mercure" >> .env
+# echo "MERCURE_URL=http://localhost:${MERCURE_PORT}" >> .env
+echo "MERCURE_URL=http://mercure:80/." >> .env
+echo "MERCURE_PUBLIC_URL=http://localhost:80/.well-known/mercure" >> .env
+echo "MERCURE_PUBLISHER_JWT_KEY=${MERCURE_PUBLISHER_JWT_KEY}" >> .env
+echo "MERCURE_SUBSCRIBER_JWT_KEY=${MERCURE_SUBSCRIBER_JWT_KEY}" >> .env
+echo "MERCURE_PORT=${MERCURE_PORT}" >> .env
+echo "APP_SECRET=${APP_SECRET}" >> .env
+echo "MERCURE_JWT_SECRET=${MERCURE_JWT_SECRET}" >> .env
 
 # Lancer l'instance avec le fichier .env généré
 docker-compose -p $PROJECT_NAME  up --build
