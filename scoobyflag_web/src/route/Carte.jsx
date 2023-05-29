7dc09f34d2fc231a01d3049326f0494cb773f25d
lucas.francois <lucas.francois@etu.unistra.fr>
2023-05-29 16:23:55 +0200

    Création de config : Gestion des équipes, nouveau système de modification/suppression

--- scoobyflag_web/src/route/Carte.jsx
+++ scoobyflag_web/src/route/Carte.jsx
@@ -1,23 +1,18 @@
 import {
   MapContainer,
   TileLayer,
-  FeatureGroup,
-  Marker,
-  Popup,
+  FeatureGroup
 } from "react-leaflet";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import L, { popup } from "leaflet";
+import L from "leaflet";
 import "../css/carte.css";
-import "../css/config.css";
 import mechant1 from "../assets/mechant1.png";
 import mechant2 from "../assets/mechant2.png";
 import loupe from "../assets/loupe.png";
 import lunettes from "../assets/lunettes.png";
 import sac from "../assets/sac.png";
 import ghost from "../assets/ghost.png";
-import player from "../assets/points.png";
-import Config from "../components/Config";
 import { EditControl } from "react-leaflet-draw";
 import pointInPolygon from "point-in-polygon";
 import ItemEquipe from "../components/ItemEquipe";
@@ -28,49 +23,17 @@ export default function Carte() {
   const [latitude, setLatitude] = useState(0.0);
   const [longitude, setLongitude] = useState(0.0);
   const [status, setStatus] = useState("");
-  const [partieLancee, setPartieLancee] = useState(false);
-
   const [zoneJeu, setZoneJeu] = useState([]);
   const [zonesInterdites, setZonesInterdites] = useState([]);
   const [mechants, setMechants] = useState([]);
   const [items, setItems] = useState([]);
-  const [joueurs, setJoueurs] = useState([]);
-  const [equipes, setEquipes] = useState([{ id: 0, nom: "equipe", nbJoueur: 5 }]);
+  const [equipes, setEquipes] = useState([{ id: 0, nom: "", nbJoueur: 1 }, { id: 1, nom: "", nbJoueur: 1 }]);
+
+  const mapRef = useRef(null);
 
   useEffect(() => {
     getLocation();
   }, []);
-
-  useEffect(() => {
-    /*const url = new URL("http://hugoslr.fr:16640/.well-known/mercure");
-    url.searchParams.append(
-      "topic",
-      "https://scoobyflag/user/{userId}".replace(
-        "{userId}",
-        0
-      )
-    );
-
-    const eventSource = new EventSource(url);
-
-    eventSource.onmessage = ({ data }) => {
-      console.log(data);
-      const user = JSON.parse(JSON.parse(data));
-      const alreadyExist = joueurs.find((u) => u.id == user.id);
-
-      if (!alreadyExist) {
-        ajouterJoueur(user.id, [user.latitude, user.longitude]);
-      } else {
-        deplacerJoueur(user.id, [user.latitude, user.longitude]);
-      }
-
-      // else if (data.includes("deplacer")) {
-      //     deplacerJoueur(0, [Math.random() * (48.54 - 48.53) + 48.53, Math.random() * (7.74 - 7.73) + 7.73]);
-      // }
-    };
-
-    return () => eventSource.close();*/
-  }, [joueurs]);
 
   const getLocation = () => {
     if (!navigator.geolocation) {
@@ -81,13 +44,13 @@ export default function Carte() {
       setLongitude(7.735647777777776);
       setStatus(null);
       /*navigator.geolocation.getCurrentPosition((position) => {
-                setLatitude(position.coords.latitude);
-                setLongitude(position.coords.longitude);
-                console.log(position.coords);
-                setStatus(null);
-            }, () => {
-                setStatus("Impossible de récupérer votre position");
-            });*/
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+        console.log(position.coords);
+        setStatus(null);
+      }, () => {
+        setStatus("Impossible de récupérer votre position");
+      });*/
     }
   };
 
@@ -127,22 +90,39 @@ export default function Carte() {
     iconAnchor: [25, 25],
   });
 
-  const playerIcon = new L.icon({
-    iconUrl: player,
-    iconSize: [25, 25],
-    iconAnchor: [12.5, 12.5],
-  });
-
-  const openConfig = () => {
-    document.querySelector(".menuConfig").classList.add("slideIn");
-    document.querySelector(".menuConfig").classList.remove("slideOut");
-  };
-
-  const clickUser = () => {
-    navigate("/user");
-  }
-
   const onCreated = (e) => {
+    //Ajout du popUp Modifier/Supprimer
+    var container = L.DomUtil.create("div");
+    var btnModifier = L.DomUtil.create("button", "", container);
+    btnModifier.innerHTML = "Modifier";
+    var btnSupprimer = L.DomUtil.create("button", "", container);
+    btnSupprimer.innerHTML = "Supprimer";
+    e.layer.bindPopup(L.popup({
+      content: container,
+      offset: L.point(0, -25)
+    }));
+    L.DomEvent.addListener(btnModifier, "click", function () {
+      mapRef.current.closePopup();
+      clickEdit();
+    })
+    L.DomEvent.addListener(btnSupprimer, "click", function (f) {
+      clickSupprimer();
+      mapRef.current.closePopup();
+      var a = document.createEvent("Event");
+      a.initEvent("click", true, true);
+      var docFragment = document.createDocumentFragment();
+      if (e.layerType === "polygon") {
+        docFragment.appendChild(e.layer._path);
+      }
+      else {
+        docFragment.appendChild(e.layer._icon);
+      }
+      var myHTMLCollection = docFragment.children;
+      !myHTMLCollection[0].dispatchEvent(a);
+      clickCancelSupprimer();
+    })
+
+    //Si zone de jeu ou zone interdite
     if (e.layerType === "polygon") {
       if (e.layer.options.color == "red") {
         var tabTemp = zonesInterdites;
@@ -153,38 +133,9 @@ export default function Carte() {
         tabTemp[0] = e.layer.getLatLngs()[0];
         setZoneJeu(tabTemp);
       }
-      e.layer._path.addEventListener("click", () => {
-        clickEdit();
-      })
-    } else {
-      e.layer.bindPopup("salut").openPopup();
-      /*e.layer._icon.addEventListener("click", () => {
-        clickEdit();
-      })*/
-
-      var ok = true;
-      var tabZone = [];
-
-      for (var i = 0; i < zonesInterdites.length; i++) {
-        tabZone = [];
-        for (var j = 0; j < zonesInterdites[i].length; j++) {
-          tabZone.push(Object.values(zonesInterdites[i][j]));
-        }
-        if (pointInPolygon([e.layer.getLatLng().lat, e.layer.getLatLng().lng], tabZone)) {
-          ok = false;
-        }
-      }
-
-      tabZone = [];
-      for (var k = 0; k < zoneJeu[0].length; k++) {
-        tabZone.push(Object.values(zoneJeu[0][k]));
-      }
-      if (!pointInPolygon([e.layer.getLatLng().lat, e.layer.getLatLng().lng], tabZone)) {
-        ok = false;
-      }
-
-      console.log(ok);
-
+      console.log(zonesInterdites);
+    }
+    else { //Sinon marker mechant ou item
       if (e.layer._icon.attributes.src.nodeValue == "/src/assets/mechant1.png") {
         var tabTemp = mechants;
         tabTemp.push(e.layer.getLatLng());
@@ -226,6 +177,29 @@ export default function Carte() {
         });
         setItems(tabTemp);
       }
+
+      /*var ok = true;
+      var tabZone = [];
+
+      for (var i = 0; i < zonesInterdites.length; i++) {
+        tabZone = [];
+        for (var j = 0; j < zonesInterdites[i].length; j++) {
+          tabZone.push(Object.values(zonesInterdites[i][j]));
+        }
+        if (pointInPolygon([e.layer.getLatLng().lat, e.layer.getLatLng().lng], tabZone)) {
+          ok = false;
+        }
+      }
+
+      tabZone = [];
+      for (var k = 0; k < zoneJeu[0].length; k++) {
+        tabZone.push(Object.values(zoneJeu[0][k]));
+      }
+      if (!pointInPolygon([e.layer.getLatLng().lat, e.layer.getLatLng().lng], tabZone)) {
+        ok = false;
+      }
+
+      console.log(ok);*/
     }
   };
 
@@ -266,7 +240,10 @@ export default function Carte() {
   };
 
   const clickEdit = (e) => {
-    //document.getElementById("detailsEdit").setAttribute("style", "visibility:visible");
+    document.getElementById("btnSave").style.display = "none";
+    document.getElementById("btnConfirmer").style.display = "block";
+    document.getElementById("btnAnnuler").style.display = "block";
+
     var e = document.createEvent("Event");
     e.initEvent("click", true, true);
     var cb = document.getElementsByClassName("leaflet-draw-edit-edit");
@@ -274,7 +251,6 @@ export default function Carte() {
   };
 
   const clickSupprimer = (e) => {
-    document.getElementById("detailsSuppr").setAttribute("style", "visibility:visible");
     var e = document.createEvent("Event");
     e.initEvent("click", true, true);
     var cb = document.getElementsByClassName("leaflet-draw-edit-remove");
@@ -289,16 +265,11 @@ export default function Carte() {
     return !cb.dispatchEvent(e);
   }
 
-  const clickSaveSupprimer = (e) => {
-    document.getElementById("detailsSuppr").setAttribute("style", "visibility:hidden");
-    var e = document.createEvent("Event");
-    e.initEvent("click", true, true);
-    var cb = document.getElementsByClassName("leaflet-draw-actions-bottom")[0].children[0].children[0];
-    return !cb.dispatchEvent(e);
-  }
-
   const clickCancelEdit = (e) => {
-    document.getElementById("detailsEdit").setAttribute("style", "visibility:hidden");
+    document.getElementById("btnSave").style.display = "block";
+    document.getElementById("btnConfirmer").style.display = "none";
+    document.getElementById("btnAnnuler").style.display = "none";
+
     var e = document.createEvent("Event");
     e.initEvent("click", true, true);
     var cb = document.getElementsByClassName("leaflet-draw-actions-top")[0].children[1].children[0];
@@ -306,7 +277,10 @@ export default function Carte() {
   }
 
   const clickSaveEdit = (e) => {
-    document.getElementById("detailsEdit").setAttribute("style", "visibility:hidden");
+    document.getElementById("btnSave").style.display = "block";
+    document.getElementById("btnConfirmer").style.display = "none";
+    document.getElementById("btnAnnuler").style.display = "none";
+
     var e = document.createEvent("Event");
     e.initEvent("click", true, true);
     var cb = document.getElementsByClassName("leaflet-draw-actions-top")[0].children[0].children[0];
@@ -329,38 +303,52 @@ export default function Carte() {
     return !cb[index].dispatchEvent(e);
   }
 
-  const ajouterJoueur = (id, coordonnees) => {
-    //setJoueurs([...joueurs, { id: joueurs.length, coordonnees: coordonnees }])
-    setJoueurs((oldJoueurs) => [
-      ...oldJoueurs,
-      { id: id, coordonnees: coordonnees },
-    ]);
-  };
+  function saveConfig() {
+    //Verif points dans la zone, equipes, ...
 
-  const deplacerJoueur = (idJoueur, coordonnees) => {
-    setJoueurs((cur) =>
-      cur.map((joueur, i) => {
-        if (joueur.id === idJoueur) {
-          return { id: joueur.id, coordonnees: coordonnees };
-        } else {
-          return joueur;
-        }
-      })
-    );
-    //setJoueurs([...joueurs, { id: idJoueur, coordonnees: coordonnees }])
-  };
+    //Pop up pour le nom de la config ?
 
-  function createTemplate() {
+    console.log({
+      name: "Sprint 1", //A CHANGER
+      modeDeJeu: "", //?
+      limitTime: 600, //?
+      teams: equipes,
+      authorizedZone: zoneJeu,
+      unauthorizedZone: zonesInterdites,
+      mechants: mechants,
+      items: items,
+      private: true, //A CHANGER
+      idCreator: JSON.stringify(JSON.parse(localStorage.getItem("user")).id)
+    })
+
     navigate("/dashboard");
   }
 
   function addEquipe() {
-    setEquipes([...equipes, { id: equipes.length, nom: "equipe 1", nbJoueur: 4 }]);
+    setEquipes([...equipes, { id: equipes.length, nom: "", nbJoueur: 1 }]);
+
+    setTimeout(() => {
+      document.querySelector(".listeEquipes").scrollTop = document.querySelector(".listeEquipes").scrollHeight;
+    }, 50);
   }
 
   function deleteEquipe(index) {
-    console.log(equipes);
     setEquipes(equipes.filter(a => a.id !== index));
+  }
+
+  function updateEquipe(index, nom, nbJoueur) {
+    setEquipes(equipes.map((equipe, i) => {
+      if (i == index) {
+        return {
+          id: index,
+          nom: nom,
+          nbJoueur: nbJoueur
+        }
+      }
+      else {
+        return equipe;
+      }
+    }))
   }
 
   async function createGame(modeJeu, listeEquipe) {
@@ -445,7 +433,11 @@ export default function Carte() {
 
   return status == null ? (
     <>
-      <MapContainer center={[latitude, longitude]} zoom={16}>
+      <MapContainer
+        center={[latitude, longitude]}
+        zoom={16}
+        ref={mapRef}
+      >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -559,117 +551,99 @@ export default function Carte() {
             }}
           />
         </FeatureGroup>
-        {partieLancee ? (
-          <>
-            {joueurs.map((joueur, id) => {
-              return (
-                <Marker
-                  key={id}
-                  position={joueur.coordonnees}
-                  icon={playerIcon}
-                >
-                  <Popup>ID Joueur : {joueur.id}</Popup>
-                </Marker>
-              );
-            })}
-            <div onClick={test} className="btnTest"></div>
-          </>
-        ) : (
-          <></>
-        )}
       </MapContainer>
-      {!partieLancee ? (
-        <>
-          <div className="sideBar">
-            <div className="divBtnSideBar">
-              <div
-                id="btnZone"
-                onClick={(e) => clickZoneJeu(e)}
-                className="btnSideBar"
-              >
-                <img src="gaming-zone.png" alt="" className="iconBar" />
-                Zone de jeu
-              </div>
-            </div>
-            <div className="divBtnSideBar">
-              <div
-                id="btnZoneInterdite"
-                onClick={(e) => clickZoneInterdite(e)}
-                className="btnSideBar"
-              >
-                <img src="forbidden.png" alt="" className="iconBar" />
-                Zone interdite
-              </div>
-            </div>
-            <div className="divBtnSideBar">
-              <div
-                id="btnFlag"
-                onClick={(e) => clickMechant(e)}
-                className="btnSideBar"
-              >
-                <img src="villain.png" alt="" className="iconBar" />
-                Méchants
-              </div>
+      <div className="sideBar">
+        <div className="divBtnSideBar">
+          <div
+            id="btnZone"
+            onClick={(e) => clickZoneJeu(e)}
+            className="btnSideBar"
+          >
+            <img src="gaming-zone.png" alt="" className="iconBar" />
+            Zone de jeu
+          </div>
+        </div>
+        <div className="divBtnSideBar">
+          <div
+            id="btnZoneInterdite"
+            onClick={(e) => clickZoneInterdite(e)}
+            className="btnSideBar"
+          >
+            <img src="forbidden.png" alt="" className="iconBar" />
+            Zone interdite
+          </div>
+        </div>
+        <div className="divBtnSideBar">
+          <div
+            id="btnFlag"
+            onClick={(e) => clickMechant(e)}
+            className="btnSideBar"
+          >
+            <img src="villain.png" alt="" className="iconBar" />
+            Méchants
+          </div>
 
-              <div className="detail" id="detailsMechants">
-                <div className="btnDetail" onClick={(e) => chooseMechant(e, 0)}>
-                  <img src="mechant1.png" alt="" className="iconBar" />
-                  Méchant 1
-                </div>
-                <div className="btnDetail" onClick={(e) => chooseMechant(e, 1)}>
-                  <img src="mechant2.png" alt="" className="iconBar" />
-                  Méchant 2
-                </div>
-              </div>
+          <div className="detail" id="detailsMechants">
+            <div className="btnDetail" onClick={(e) => chooseMechant(e, 0)}>
+              <img src="mechant1.png" alt="" className="iconBar" />
+              Méchant 1
             </div>
-            <div className="divBtnSideBar">
-              <div
-                id="btnItems"
-                onClick={(e) => clickObjet(e)}
-                className="btnSideBar"
-              >
-                <img src="object.png" alt="" className="iconBar" />
-                Objets
-              </div>
+            <div className="btnDetail" onClick={(e) => chooseMechant(e, 1)}>
+              <img src="mechant2.png" alt="" className="iconBar" />
+              Méchant 2
+            </div>
+          </div>
+        </div>
+        <div className="divBtnSideBar">
+          <div
+            id="btnItems"
+            onClick={(e) => clickObjet(e)}
+            className="btnSideBar"
+          >
+            <img src="object.png" alt="" className="iconBar" />
+            Objets
+          </div>
 
-              <div className="detail" id="detailsItems">
-                <div className="btnDetail" onClick={(e) => chooseItem(e, 2)}>
-                  <img src="loupe.png" alt="" className="iconBar" />
-                  Item 1
-                </div>
-                <div className="btnDetail" onClick={(e) => chooseItem(e, 3)}>
-                  <img src="lunettes.png" alt="" className="iconBar" />
-                  Item 2
-                </div>
-                <div className="btnDetail" onClick={(e) => chooseItem(e, 4)}>
-                  <img src="sac.png" alt="" className="iconBar" />
-                  Item 3
-                </div>
-                <div className="btnDetail" onClick={(e) => chooseItem(e, 5)}>
-                  <img src="ghost.png" alt="" className="iconBar" />
-                  Item 4
-                </div>
-              </div>
+          <div className="detail" id="detailsItems">
+            <div className="btnDetail" onClick={(e) => chooseItem(e, 2)}>
+              <img src="loupe.png" alt="" className="iconBar" />
+              Item 1
+            </div>
+            <div className="btnDetail" onClick={(e) => chooseItem(e, 3)}>
+              <img src="lunettes.png" alt="" className="iconBar" />
+              Item 2
+            </div>
+            <div className="btnDetail" onClick={(e) => chooseItem(e, 4)}>
+              <img src="sac.png" alt="" className="iconBar" />
+              Item 3
+            </div>
+            <div className="btnDetail" onClick={(e) => chooseItem(e, 5)}>
+              <img src="ghost.png" alt="" className="iconBar" />
+              Item 4
             </div>
           </div>
-          <div className="divEquipes">
-            <div className="divTopEquipes">
-              <div className="titreEquipe">Équipes</div>
-              <img src="scooby.png" className="btnAddEquipe" onClick={addEquipe}></img>
-            </div>
-            <div className="listeEquipes">
-              {equipes.map((equipe, index) => {
-                return <ItemEquipe equipe={equipe} key={index} deleteEquipe={() => deleteEquipe(equipe.id)} />
-              })}
-            </div>
-          </div>
-          <div className="btnCreate" onClick={createTemplate}>
-            Sauvegarder la configuration
-          </div>
-        </>
-      ) : (
-        <></>
-      )}
+        </div>
+      </div>
+      <div className="divEquipes">
+        <div className="divTopEquipes">
+          <div className="titreEquipe">Équipes</div>
+          <img src="add.png" className="btnAddEquipe" onClick={addEquipe}></img>
+        </div>
+        <div className="listeEquipes">
+          {equipes.map((equipe, index) => {
+            return <ItemEquipe equipe={equipe} key={index} deleteEquipe={() => deleteEquipe(equipe.id)} updateEquipe={updateEquipe} />
+          })}
+        </div>
+      </div>
+      <div className="btnCarte" id="btnSave" onClick={saveConfig}>
+        Sauvegarder la configuration
+      </div>
+      <div className="btnCarte" id="btnConfirmer" onClick={clickSaveEdit}>
+        Confirmer les modifications
+      </div>
+      <div className="btnCarte" id="btnAnnuler" onClick={clickCancelEdit}>
+        Annuler les modifications
+      </div>
     </>
   ) : (
     <h1>{status}</h1>
